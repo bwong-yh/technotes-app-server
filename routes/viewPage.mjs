ef7ffa73546bc9cd5c9ef685e917677ca573e712
Billy Wong <bwong.yh@protonmail.com>
2022-09-24 15:07:21 +0900

    install dependencies

--- routes/viewPage.mjs
+++ routes/viewPage.mjs
@@ -3,25 +3,18 @@ import path from 'path';
 import getDirname from '../utils/getDirname.mjs';
 
 const router = express.Router();
+const __dirname = getDirname(import.meta.url);
 
 router.get('/about(.html)?', (req, res) => {
-  res.sendFile(
-    path.join(getDirname(import.meta.url), '..', 'views', 'about.html')
-  );
+  res.sendFile(path.join(__dirname, '..', 'views', 'about.html'));
 });
 
 router.get('^/$|/index(.html)?', (req, res) => {
-  res.sendFile(
-    path.join(getDirname(import.meta.url), '..', 'views', 'index.html')
-  );
+  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
 router.get('/*', (req, res) => {
-  res
-    .status(404)
-    .sendFile(
-      path.join(getDirname(import.meta.url), '..', 'views', '404.html')
-    );
+  res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'));
 });
 
 export default router;
--- server.mjs
+++ server.mjs
@@ -1,15 +1,19 @@
 import express from 'express';
-import path from 'path';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
 import viewRouter from './routes/viewPage.mjs';
-import getDirname from './utils/getDirname.mjs';
-
-const __dirname = getDirname(import.meta.url);
+import { logger } from './middleware/logger.mjs';
 
 const app = express();
 const PORT = process.env.PORT || 8003;
 
-// app.use to serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+// middlewares
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// routes
 app.use(viewRouter);
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
